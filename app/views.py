a6948aca99706dba8b386219076cc54c494501df
Alexander Ogurtsov <aogurzow@gmail.com>
2017-10-08 12:21:22 +0300

    added profile page

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 from flask_login import current_user
 
 
 @app.route('/')
 def main():
     if current_user.is_authenticated:
-        return render_template('main/profile.html')
+        return redirect('/profile')
     else:
         return render_template('main/landing.html')
 
@@ -16,3 +16,7 @@ def main():
 def settings():
     return render_template('main/settings.html')
 
+
+@app.route('/profile')
+def profile():
+    return render_template('main/profile.html')
